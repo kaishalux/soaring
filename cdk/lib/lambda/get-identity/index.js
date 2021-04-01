ef3b3d465a2a52a26fb73cb156b18f054fff0d65
soogoi <emma.soo@hotmail.com>
2021-04-01 18:29:57 +1100

    resolved more pr comments

--- cdk/lib/lambda/get-identity/index.js
+++ cdk/lib/lambda/get-identity/index.js
@@ -2,19 +2,21 @@ const aws = require('aws-sdk');
 const iam = new aws.IAM();
 
 exports.handler = async (event, context) => {
-    if (typeof(event[0].user.userIdentity.arn) == "undefined") return;
+    const response = event[0];
 
-    const username = event[0].user.userIdentity.arn.split("/").pop();
-    event[0].user.userPolicies = await getUserPolicies(username);
+    if (typeof(response.user.userIdentity.arn) == "undefined") return;
 
-    if (typeof(event[0].user.userIdentity.sessionContext) != "undefined") {
-        var role = event[0].user.userIdentity.sessionContext.sessionIssuer.userName;
-        event[0].user.rolePolicies = await getRolePolicies(role);
+    const username = response.user.userIdentity.arn.split("/").pop();
+    response.user.userPolicies = await getUserPolicies(username);
+
+    if (typeof(response.user.userIdentity.sessionContext) != "undefined") {
+        const role = response.user.userIdentity.sessionContext.sessionIssuer.userName;
+        response.user.rolePolicies = await getRolePolicies(role);
     }
 
-    event[0].user.groups = await getGroups(username);
-    for (var i = 0; i < event[0].user.groups.length; i++) {
-        event[0].user.groups[i].Policies = await getGroupPolicies(event[0].user.groups[i].GroupName);
+    response.user.groups = await getGroups(username);
+    for (const group of response.user.groups) {
+        group.Policies = await getGroupPolicies(group.GroupName);
     }
 
     return event;
@@ -22,56 +24,24 @@ exports.handler = async (event, context) => {
 
 const getGroups = async (user) => {
     const params = { UserName: user };
-    return new Promise((resolve, reject) => {
-        try {
-            iam.listGroupsForUser(params, (err, data) => {
-                if (err) return reject(err);
-                resolve(data.Groups);
-            });
-        } catch (err) {
-            reject(err);
-        }
-    });
+    const result = await iam.listGroupsForUser(params).promise();
+    return result.Groups;
 };
 
 const getUserPolicies = async (user) => {
-    var params = { UserName: user };
-    return new Promise((resolve, reject) => {
-        try {
-            iam.listAttachedUserPolicies(params, (err, data) => {
-                if (err) return reject(err);
-                resolve(data.AttachedPolicies);
-            });
-        } catch (err) {
-            reject(err);
-        }
-    });
+    const params = { UserName: user };
+    const result = await iam.listAttachedUserPolicies(params).promise();
+    return result.AttachedPolicies;
 };
 
 const getRolePolicies = async (role) => {
-    var params = { RoleName: role };
-    return new Promise((resolve, reject) => {
-        try {
-            iam.listAttachedRolePolicies(params, (err, data) => {
-                if (err) return reject(err);
-                resolve(data.AttachedPolicies);
-            });
-        } catch (err) {
-            reject(err);
-        }
-    });
+    const params = { RoleName: role };
+    const result = await iam.listAttachedRolePolicies(params).promise();
+    return result.AttachedPolicies;
 };
 
 const getGroupPolicies = async (group) => {
-    var params = { GroupName: group };
-    return new Promise((resolve, reject) => {
-        try {
-            iam.listAttachedGroupPolicies(params, (err, data) => {
-                if (err) return reject(err);
-                resolve(data.AttachedPolicies);
-            });
-        } catch (err) {
-            reject(err);
-        }
-    });
+    const params = { GroupName: group };
+    const result = await iam.listAttachedGroupPolicies(params).promise();
+    return result.AttachedPolicies;
 };
